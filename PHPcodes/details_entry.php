ba394f17ed781a915c09086e4be7080df050fe13
Afrikanacompsquare <zechariahouma@gmail.com>
2022-05-26 11:33:26 +0300

    PHPcodes

---
+++ PHPcodes/details_entry.php
@@ -0,0 +1,36 @@
+<?php
+
+$servername= "localhost";
+$username= "root";
+$password="";
+$database_name="database1";
+
+$conn=mysqli_connect($servername, $username, $password, $database_name);
+//check the connection
+
+if(! $conn)
+{
+	die("connection failed:" . mysqli_connect_error());
+}
+
+if(isset($_POST['save']))
+{
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+
+	$sql_query = "INSERT INTO entry_details(first_name, last_name, gender, email, phone)
+	VALUES ('$first_name', '$last_name', '$gender', '$email', '$phone')";
+	if (mysqli_query($conn, $sql_query))
+	{
+	echo "New details entry inserted successfully!!";
+	}
+	else
+	{
+	echo "Error:" .$sql . "" .mysqli_error($conn);
+	}
+	mysqli_close($conn);
+}
+?>
